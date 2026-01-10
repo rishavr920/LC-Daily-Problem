447806c55cd8f6b2e829a79a99e84694ab8b69c4
Rishav Raj <rishavr920@gmail.com>
2026-01-10 23:55:06 +0530

    new q/n

---
+++ January/10-1-26.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/description/
+class Solution {
+public:
+    int m,n;
+
+    int dp[1001][1001];
+
+    int solve(int i,int j,string& s1,string &s2){
+
+        //agar dono out of bound ho jaye
+        if(i>=m and j>=n){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        //agr i out of bound ho gaya,
+        //to j ka bacha kisi kaam ka ni just add as ascii
+        if(i>=m){
+            return s2[j] + solve(i,j+1,s1,s2);
+        }
+        else if(j>=n){
+            return s1[i] + solve(i+1,j,s1,s2);
+        }
+
+        if(s1[i] == s2[j]){
+            return solve(i+1,j+1,s1,s2);
+        }
+
+        // two possibilites
+        int delete_s1_i = s1[i] + solve(i+1,j,s1,s2);
+        int delete_s2_j = s2[j] + solve(i,j+1,s1,s2);
+
+        return dp[i][j] = min(delete_s1_i,delete_s2_j);
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        m = s1.size();
+        n = s2.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,s1,s2);
+    }
+};
